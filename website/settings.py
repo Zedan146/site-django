3f0b0db87bbd5121695b1e7da98cf3de7ab342cb
Даня Зенкович <141771778+Zedan146@users.noreply.github.com>
2025-02-14 12:58:42 +0300

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'danila.zenkovitch17@yandex.ru'
-EMAIL_HOST_PASSWORD = 'eupyorpfeornwxbx'
+EMAIL_HOST_USER = 'my_host_user'
+EMAIL_HOST_PASSWORD = 'my_password'
 EMAIL_USE_SSL = True
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
